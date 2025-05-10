9e591d8202c54fbb0db0ad1e10963ed34b55f27d
VojtechKycelt <71277196+VojtechKycelt@users.noreply.github.com>
2025-05-10 21:55:57 +0200

    Initial commit

---
+++ Source/AnubisUnreal/AnubisUnrealGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AnubisUnrealGameMode.h"
+#include "AnubisUnrealCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AAnubisUnrealGameMode::AAnubisUnrealGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/AnubisUnreal/AnubisUnrealGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnubisUnrealGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAnubisUnrealGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAnubisUnrealGameMode();
+};
+
+
+
